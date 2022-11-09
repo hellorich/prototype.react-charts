974f6d8205b3ab42e25ff3cc74b5585a5adc7b9c
Rich Cookson <hellorich@frobitz.com>
2022-11-09 15:05:34 +0000

    Initial working test of Pie chart

---
+++ src/components/ChartDonut/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+import { VictoryPie } from 'victory'
+
+const data = [
+  { x: "Cats", y: 35 },
+  { x: "Dogs", y: 40 },
+  { x: "Birds", y: 55 }
+]
+
+const StyledChartDonut = styled(VictoryPie)`
+
+`
+export const ChartDonut = () => {
+  return (
+    <StyledChartDonut 
+      data={data}
+    />
+  )
+}
